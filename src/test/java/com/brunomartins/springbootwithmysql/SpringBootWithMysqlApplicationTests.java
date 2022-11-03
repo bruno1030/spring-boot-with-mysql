5e129d566b4ffd7bd193df20d10adf20b30a47ee
Bruno <brunomartins1030@gmail.com>
2022-11-03 16:05:56 -0300

    first commit + creating Dockerfile

---
+++ src/test/java/com/brunomartins/springbootwithmysql/SpringBootWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomartins.springbootwithmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
