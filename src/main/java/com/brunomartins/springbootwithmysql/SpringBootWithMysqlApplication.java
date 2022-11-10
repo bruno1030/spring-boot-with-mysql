2d787a301a87af4c6eabd94fb9e626b88b586f0e
Bruno <brunomartins1030@gmail.com>
2022-11-10 11:30:44 -0300

    creating model

--- src/main/java/com/brunomartins/springbootwithmysql/SpringBootWithMysqlApplication.java
+++ src/main/java/com/brunomartins/springbootwithmysql/SpringBootWithMysqlApplication.java
@@ -2,8 +2,10 @@ package com.brunomartins.springbootwithmysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.brunomartins.springbootwithmysql")
+@EntityScan(basePackages = "com.brunomartins.springbootwithmysql.model")
 public class SpringBootWithMysqlApplication {
 
 	public static void main(String[] args) {
