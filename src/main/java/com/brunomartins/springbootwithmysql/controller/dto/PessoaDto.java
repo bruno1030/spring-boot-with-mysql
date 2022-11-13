f68653c609ed9ef0c7d6a288d31703a4e73dfcdf
Bruno <brunomartins1030@gmail.com>
2022-11-13 00:09:52 -0300

    method to convert entity to dto

--- src/main/java/com/brunomartins/springbootwithmysql/controller/dto/PessoaDto.java
+++ src/main/java/com/brunomartins/springbootwithmysql/controller/dto/PessoaDto.java
@@ -1,32 +1,25 @@
 package com.brunomartins.springbootwithmysql.controller.dto;
 
+import com.brunomartins.springbootwithmysql.model.Pessoa;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PessoaDto {
+
+    // method to convert Pessoa (entity) to PessoaDto
+    public static PessoaDto converter(Pessoa p){
+        var pessoa = new PessoaDto();
+        pessoa.id = p.getId();
+        pessoa.name = p.getNome();
+        pessoa.sobrenome = p.getSobrenome();
+
+        return pessoa;
+    }
 
     private Long id;
     private String name;
     private String sobrenome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
 }
--- src/main/java/com/brunomartins/springbootwithmysql/model/Pessoa.java
+++ src/main/java/com/brunomartins/springbootwithmysql/model/Pessoa.java
@@ -1,9 +1,14 @@
 package com.brunomartins.springbootwithmysql.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "pessoa")
+@Getter
+@Setter
 public class Pessoa {
 
     @Id
@@ -17,27 +22,4 @@ public class Pessoa {
     @Column(name="sobrenome")
     private String sobrenome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
 }
